4e7f2b7d65122bc9572344699daa915b20e86009
rub1-618 <thompson.alan618@gmail.com>
2026-08-19 22:54:20 +0300

    Optimized add-loc command's process.

--- src/lpnl_add_loc.rs
+++ src/lpnl_add_loc.rs
@@ -4,6 +4,11 @@ use crate::validation::{get_domain, get_location, get_proxy, get_root};
 use crate::commands::{proceed_nginx, proceed_check_nginx_tmp};
 use std::{io, fs, path::PathBuf};
 
+#[derive(Debug, Clone)]
+enum LocMode {
+    Root, Proxy
+}
+
 pub fn add_loc( // todo: sites-disabled dir checking and proceeding
     domain: Option<String>, nloc: Option<String>, 
     nroot: Option<PathBuf>, nproxy: Option<String>
@@ -66,30 +71,30 @@ pub fn add_loc( // todo: sites-disabled dir checking and proceeding
     if nproxy.is_none() && nroot.is_none() {
         if is_proxy_mode()? {
             let proxy = get_proxy(nproxy)?;
-            return add_proxy_loc(location, proxy, target_dir);
+            return proceed_add_loc(location, proxy, target_dir, LocMode::Proxy);
         } else {
             let root  = get_root(nroot.clone())?;
-            return add_root_loc(location, root, target_dir);
+            return proceed_add_loc(location, root, target_dir, LocMode::Root);
         }
     }
 
     if nroot.is_some() {
         let root  = get_root(nroot.clone())?;
-        return add_root_loc(location, root, target_dir);
+            return proceed_add_loc(location, root, target_dir, LocMode::Root);
     };
 
     if nproxy.is_some() {
         let proxy  = get_proxy(nproxy.clone())?;
-        return add_proxy_loc(location, proxy, target_dir);
+        return proceed_add_loc(location, proxy, target_dir, LocMode::Proxy);
     };
 
     Err(LpnlError::AddLocError { 
-        message: "Root / proxy selection stage went wrong.".to_string(),
+        message: "Root/proxy selection stage went wrong.".to_string(),
         kind: AddLocErrorKind::NotFound
     })
 }
 
-fn add_root_loc(location: String, root: String, target_dir: PathBuf) -> Result<(), LpnlError> {
+fn proceed_add_loc(location: String, data: String, target_dir: PathBuf, mode: LocMode) -> Result<(), LpnlError> {
 
    let target_conf_str = target_dir.to_str().unwrap().to_string(); 
     let target_conf_as_str = match fs::read_to_string(&target_dir) {
@@ -104,10 +109,25 @@ fn add_root_loc(location: String, root: String, target_dir: PathBuf) -> Result<(
     if target_conf_as_str.lines().find(|&l| l.contains(&conf_loc)).is_none() {
         match target_conf_as_str.lines().find(|&l| l.trim().starts_with("location / {")) {
             Some(l) => {
-                let conf_part = format!("
-                location {location} {{ root {root}; }}
+                let conf_part = match mode {
+                    LocMode::Root  => {
+                        format!("
+                location {location} {{ root {data}; }}
                 
-                location / {{");
+                location / {{")
+                    }
+                    LocMode::Proxy => {
+                        format!("
+                location {location} {{
+                    proxy_pass {data};
+                    proxy_set_header Host $host;
+                    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                    proxy_set_header X-Real-IP $remote_addr;
+                }}
+                
+                location / {{")
+                    }
+                };
                 let final_conf = target_conf_as_str.replace(l, &conf_part);
                 let test_conf = format!("events {{  }} http {{ {final_conf} }}");
                 proceed_check_nginx_tmp(&test_conf)?;
@@ -134,59 +154,7 @@ fn add_root_loc(location: String, root: String, target_dir: PathBuf) -> Result<(
     }
 
 
-    Ok(println!("Location '{location}' with its root '{root}' created."))
-}
-
-fn add_proxy_loc(location: String, proxy: String, target_dir: PathBuf) -> Result<(), LpnlError> {
-
-    let target_conf_str = target_dir.to_str().unwrap().to_string(); 
-    let target_conf_as_str = match fs::read_to_string(&target_dir) {
-        Ok(s) => s,
-        Err(e) => return Err(LpnlError::AddLocError { 
-            message: format!("Unable to read the '{target_conf_str}' to get the config: {e}"),
-            kind: AddLocErrorKind::FsFailure
-        })
-    };
-
-    let conf_loc = format!("location {location}");
-    if target_conf_as_str.lines().find(|&l| l.contains(&conf_loc)).is_none() {
-        match target_conf_as_str.lines().find(|&l| l.trim().starts_with("location / {")) {
-            Some(l) => {
-                let conf_part = format!("
-                location {location} {{
-                    proxy_pass {proxy};
-                    proxy_set_header Host $host;
-                    proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-                    proxy_set_header X-Real-IP $remote_addr;
-                }}
-                
-                location / {{");
-                let final_conf = target_conf_as_str.replace(l, &conf_part);
-                let test_conf = format!("events {{  }} http {{ {final_conf} }}");
-                proceed_check_nginx_tmp(&test_conf)?;
-                match fs::write(&target_conf_str, &final_conf) {
-                    Ok(_) => {
-                        proceed_nginx()?;
-                    }
-                    Err(e) => return Err(LpnlError::AddLocError { 
-                        message: format!("Unable to update the config in '{target_conf_str}': {e}"),
-                        kind: AddLocErrorKind::FsFailure
-                    })
-                }
-            },
-            None => return Err(LpnlError::AddLocError { 
-                message: format!("Unable to find the 'location /' block in '{target_conf_str}'."),
-                kind: AddLocErrorKind::NotFound
-            })
-        }
-    } else {
-        return Err(LpnlError::AddLocError { 
-            message: format!("The config location '{location}' already exists."),
-            kind: AddLocErrorKind::AlreadyExists
-        })
-    }
-
-    Ok(println!("Location '{location}' with its proxy '{proxy}' created."))
+    Ok(println!("Location '{location}' with the content of '{data}' created."))
 }
 
 fn is_proxy_mode() -> Result<bool, LpnlError> {
